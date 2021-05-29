14f9e4d4262588db5c2e3d05288031a1aed5dcf9
Patrick Drechsler <patrick@pdrechsler.de>
2021-05-29 20:20:55 +0200

    extract ui stuff for dom maninpulation

--- js/script.js
+++ js/script.js
@@ -31,31 +31,3 @@ function update_shipping_icons() {
         }
     }
 }
-
-// DOM functions -----------------------------------
-function set_cart_total_dom() {
-    var element = document.getElementById("shoppingCartTotal")
-    element.innerText = shopping_cart_total;
-}
-
-function get_buy_buttons_dom() {
-    var buttons = document.getElementsByClassName('btnItem');
-    return buttons;
-}
-
-function btnToItem(btn) {
-    var productPrice = parseInt(btn.querySelector('.productPrice').innerText);
-    return {
-        price: productPrice
-    };
-}
-
-function show_free_shipping_icon(button) {
-    var freeShipping = button.querySelector('.freeShipping')
-    freeShipping.style.display = 'inline-block';
-}
-
-function hide_free_shipping_icon(button) {
-    var freeShipping = button.querySelector('.freeShipping')
-    freeShipping.style.display = 'none';
-}
