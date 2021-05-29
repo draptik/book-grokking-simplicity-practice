7398e14937d259906ae097399698643efc41e261
Patrick Drechsler <patrick@pdrechsler.de>
2021-05-29 20:39:07 +0200

    move update_shipping_icons to ui

--- js/script.js
+++ js/script.js
@@ -18,16 +18,3 @@ function calc_cart_total() {
     set_cart_total_dom();
     update_shipping_icons();
 }
-
-function update_shipping_icons() {
-    var buy_buttons = get_buy_buttons_dom();
-    for (let i = 0; i < buy_buttons.length; i++) {
-        var button = buy_buttons[i];
-        var item = btnToItem(button);
-        if (item.price + shopping_cart_total >= 20) {
-            show_free_shipping_icon(button);
-        } else {
-            hide_free_shipping_icon(button);
-        }
-    }
-}
--- js/ui-stuff.js
+++ js/ui-stuff.js
@@ -24,3 +24,16 @@ function hide_free_shipping_icon(button) {
     var freeShipping = button.querySelector('.freeShipping')
     freeShipping.style.display = 'none';
 }
+
+function update_shipping_icons() {
+    var buy_buttons = get_buy_buttons_dom();
+    for (let i = 0; i < buy_buttons.length; i++) {
+        var button = buy_buttons[i];
+        var item = btnToItem(button);
+        if (item.price + shopping_cart_total >= 20) {
+            show_free_shipping_icon(button);
+        } else {
+            hide_free_shipping_icon(button);
+        }
+    }
+}
