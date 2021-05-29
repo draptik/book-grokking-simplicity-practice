c3e8b5c337f5e87887fc16792f84178b3d248d3e
Patrick Drechsler <patrick@pdrechsler.de>
2021-05-29 20:46:47 +0200

    calc_total: extract calculatin from action

--- js/script.js
+++ js/script.js
@@ -10,18 +10,19 @@ function add_item_to_cart(name, price) {
 }
 
 function calc_cart_total() {
-    calc_total();
+    shopping_cart_total = calc_total();
     set_cart_total_dom();
     update_shipping_icons();
     update_tax_dom();
 }
 
 function calc_total() {
-    shopping_cart_total = 0;
+    var total = 0;
     for (let i = 0; i < shopping_cart.length; i++) {
         var item = shopping_cart[i];
-        shopping_cart_total += item.price;
+        total += item.price;
     }
+    return total;
 }
 
 function update_tax_dom() {
