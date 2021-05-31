da173147ca64ec56a76d3517bff503d6b9c5376f
Patrick Drechsler <patrick@pdrechsler.de>
2021-05-31 18:26:50 +0200

    cleanup

--- js/script.js
+++ js/script.js
@@ -9,13 +9,13 @@ function add_element_last(array, elem) {
 
 // Custom ----------------------------------------
 function add_item_to_cart(name, price) {
-    shopping_cart = add_item(shopping_cart, make_cart_item(name, price));
+    var item = make_cart_item(name, price)
+    shopping_cart = add_item(shopping_cart, item);
 
     var total = calc_total(shopping_cart);
     set_cart_total_dom(total);
     update_shipping_icons(shopping_cart);
     update_tax_dom(total);
-    shopping_cart_total = total;
 }
 
 function add_item(cart, item) {
